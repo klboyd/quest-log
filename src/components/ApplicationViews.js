7274d7267810c3c86469305b7466a74cb5666ce1
Ken Boyd <klb417@gmail.com>
2019-12-16 12:02:59 -0600

    can log user in with basic validation

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,19 +2,47 @@ import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import CharacterForm from "./characters/CharacterForm";
 import QuestViews from "./quest/QuestViews";
+import Login from "./auth/Login";
 
 export default class ApplicationViews extends Component {
   render() {
+    console.log("applicationViews props", this.props);
     return (
       <>
-        <Route exact path="/" render={props => <Redirect to="/quests" />} />
+        <Route exact path="/login" render={props => <Login {...props} />} />
+        <Route
+          exact
+          path="/"
+          render={props =>
+            this.props.isAuthenticated() ? (
+              <Redirect to="/quests" />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
 
-        <Route path="/quests" render={props => <QuestViews {...props} />} />
+        <Route
+          path="/quests"
+          render={props =>
+            this.props.isAuthenticated() ? (
+              <QuestViews {...props} />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
+        />
 
         <Route
           exact
           path="/character/new"
-          render={props => <CharacterForm {...props} />}
+          render={props =>
+            this.props.isAuthenticated() ? (
+              <CharacterForm {...props} />
+            ) : (
+              <Redirect to="/login" />
+            )
+          }
         />
         {/* <Route path="/guild" render={props => <Guild />} /> */}
       </>
